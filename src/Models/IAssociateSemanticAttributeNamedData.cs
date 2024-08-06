55c0d69995f37677efd77b8b89e2ddc5e0a9d476
ErikWe <erikwestberg7@gmail.com>
2024-08-06 15:03:19 +0200

    Upt dependencies to use marker-interfaces

--- src/Models/IAssociateSemanticAttributeNamedData.cs
+++ src/Models/IAssociateSemanticAttributeNamedData.cs
@@ -2,10 +2,13 @@
 
 using Microsoft.CodeAnalysis;
 
+using Paraminter.Associators.Models;
+
 using System.Collections.Generic;
 
 /// <summary>Represents data used to associate semantic named attribute arguments.</summary>
 public interface IAssociateSemanticAttributeNamedData
+    : IAssociateArgumentsData
 {
     /// <summary>The names of named attribute parameters and the associated arguments.</summary>
     public abstract IReadOnlyList<KeyValuePair<string, TypedConstant>> Associations { get; }
